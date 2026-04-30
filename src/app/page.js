176afb592f975f9ae56836a93fd800c30bcae652
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-04-30 22:37:06 +0600

    data.json added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import PopularCoursesSection from "@/components/PopularCoursesSection";
 import { Button } from "@heroui/react";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <Banner/>
+      <PopularCoursesSection/>
     </div>
   );
 }
