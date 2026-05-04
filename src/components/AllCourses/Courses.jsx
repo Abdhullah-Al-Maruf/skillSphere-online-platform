a3919c9fafb3dfb718640ef299cc42fb7ccffe24
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-04 09:33:28 +0600

    btn added

--- src/components/AllCourses/Courses.jsx
+++ src/components/AllCourses/Courses.jsx
@@ -1,18 +1,15 @@
+"use client";
 
+import { useState } from "react";
+import CourseCard from "../ui/Cards/CourseCard";
+import { Button } from "@heroui/react";
 
-'use client';
-
-
-import { useState } from 'react';
-import CourseCard from '../ui/Cards/CourseCard';
-
-
-export default function Courses( {courses}) {
-  const [searchQuery, setSearchQuery] = useState('');
+export default function Courses({ courses }) {
+  const [searchQuery, setSearchQuery] = useState("");
 
   // Filter courses based on search query
   const filteredCourses = courses.filter((course) =>
-    course.title.toLowerCase().includes(searchQuery.toLowerCase())
+    course.title.toLowerCase().includes(searchQuery.toLowerCase()),
   );
 
   return (
@@ -41,23 +38,21 @@ export default function Courses( {courses}) {
           className="flex-1 py-4 px-2 bg-transparent border-none outline-none text-gray-700 placeholder-gray-400"
         />
 
-        {/* <button className="  mx-2 px-8 py-2.5 bg-orange-500 hover:bg-orange-600 text-white font-medium rounded-full transition-colors">
+        <Button size="lg" className=" hidden sm:block sm:mx-2 py-2 px-2 md:mx-2 md:px-8 md:py-2.5 bg-linear-to-r from-[#ff8a00] to-[#ff3d00] transition-color cursor-pointer text-white font-medium rounded-full">
           Find Course
-        </button> */}
+        </Button>
       </div>
 
       {/* Render Filtered Results */}
-      {
-filteredCourses.length===0?     <p className="text-gray-500 text-center mt-4">No courses found</p>
-  :
-   <div className="grid mt-4 mb-20 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {filteredCourses.map((data) => (
-          <CourseCard key={data.id} data={data} />
-        ))}
-      </div>
-
-      }
-    
-      </div>
+      {filteredCourses.length === 0 ? (
+        <p className="text-gray-500 text-center mt-4">No courses found</p>
+      ) : (
+        <div className="grid mt-4 mb-20 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {filteredCourses.map((data) => (
+            <CourseCard key={data.id} data={data} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
